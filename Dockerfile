1dfea93beaf09bee8ed46a026fa8e604d2e516c2
Eli Streit <elistreit@outlook.com>
2017-04-21 12:32:45 +0300

    Some changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
-FROM elistreit/node:latest-onbuild
+FROM elistreit/node:onbuild
 
-EXPOSE 3000:3000
+LABEL name "webhook-ml"
+LABEL description "Webhook gateway to MailerLite API (v2)"
+LABEL version "1.0.0"
+LABEL maintainer "cloudever"
 
-RUN yarn global add pm2
+ARG PORT=3000
+ENV PORT $PORT
+
+ARG PORTEXT=80
+ENV PORTEXT $PORTEXT
+
+ARG ECHO="printf \n\n\033[1;30m[Dockerfile]\t\033[1;32m%s\033[0m\n\n\n"
+
+EXPOSE $PORTEXT:$PORT
+
+RUN \
+  $ECHO "Install PM2 package global" \
+  ; yarn global add pm2
 
 ENTRYPOINT ["pm2-docker", "start", "pm2.json"]
--- app/mailerlite/api.js
+++ app/mailerlite/api.js
@@ -11,54 +11,55 @@ const constants = require("./constants")
 const api_endpoint = constants.API_ENDPOINT
 
 class API {
-    
-    constructor (params) {
-        this.key = params.key
-        
-        this.request = request.defaults({
-            headers: {
-                'X-MailerLite-ApiKey': this.key
-            },
-            json: true
-        })
 
-    }
-    
-    call (path, method = 'get', body) {
-        
-        return new Promise ((resolve, reject) => {
-            
-            var options = {
-                url: url.resolve (api_endpoint, path),
-                method,
-                body
-            }
-                    
-            this.request[method](options, (err, res, body) => {
-                if (err) {
-                    return resolve ({ 
-                        response: {
-                            error: {
-                                message: "Network error"
-                            }
-                        }, code: 0 })
-                }
-                
-                return resolve ({ response: body, code: res.statusCode})
-            })
-        })
-    }
-    
-    get groups () {
-        return {
-            subscribers: {
-                add: async (id, params) => {
-                    return await this.call(`groups/${id}/subscribers`, 'post', params)
-                }
-            }
+  constructor(params) {
+    this.key = params.key
+
+    this.request = request.defaults({
+      headers: {
+        'X-MailerLite-ApiKey': this.key
+      },
+      json: true
+    })
+
+  }
+
+  call(path, method = 'get', body) {
+
+    return new Promise((resolve, reject) => {
+
+      var options = {
+        url: url.resolve(api_endpoint, path),
+        method,
+        body
+      }
+
+      this.request[method](options, (err, res, body) => {
+        if (err) {
+          return resolve({
+            response: {
+              error: {
+                message: "Network error"
+              }
+            }, code: 0
+          })
         }
+
+        return resolve({response: body, code: res.statusCode})
+      })
+    })
+  }
+
+  get groups() {
+    return {
+      subscribers: {
+        add: async(id, params) => {
+          return await this.call(`groups/${id}/subscribers`, 'post', params)
+        }
+      }
     }
+  }
 
 }
 
-module.exports = (params) => new API (params)
+module.exports = (params) => new API(params)
